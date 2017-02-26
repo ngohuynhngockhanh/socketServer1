aac2a60e8f69289f0967bfc95e30807b3130b287
root <ngohuynhngockhanh@gmail.com>
2017-02-26 16:52:05 +0700

    ok

---
+++ index.js
@@ -0,0 +1,43 @@
+var util = require('util');
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+app.listen(3484);
+
+//giải nén chuỗi JSON thành các OBJECT
+function ParseJson(jsondata) {
+    try {
+        return JSON.parse(jsondata);
+    } catch (error) {
+        return null;
+    }
+}
+
+//Gửi dữ liệu thông qua 
+function sendTime() {
+    io.sockets.emit('atime', {
+		khanh_dep_trai: "khanh dep trai",
+        ESP8266: 12,
+		soPi: 3.14,
+		time: new Date()
+    });
+}
+
+//Khi có mệt kết nối được tạo giữa Socket Client và Socket Server
+io.on('connection', function(socket) {
+	//hàm console.log giống như hàm Serial.println trên Arduino
+	
+    console.log("Connected"); //In ra màn hình console là đã kết nối thành công.
+	
+	//Gửi lệnh 
+    socket.emit('welcome', {
+        message: 'Connected !!!!'
+    });
+    socket.on('connection', function(data) {
+        console.log(data);
+    });
+    socket.on('atime', function(data) {
+        sendTime();
+        console.log(data);
+    });
+});
